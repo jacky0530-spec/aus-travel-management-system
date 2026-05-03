53052aabfcd0dfa9d6a13bd4048f6de537fda4fe
jacky0530-spec <jacky0530@gmail.com>
2026-05-03 14:02:16 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCBKoPGs0TW6VI1Ws3-61USpjARQR-O8Mc",
+  authDomain: "aus-travel-system.firebaseapp.com",
+  projectId: "aus-travel-system",
+  storageBucket: "aus-travel-system.firebasestorage.app",
+  messagingSenderId: "733398393106",
+  appId: "1:733398393106:web:370b0717ef760be29953cb",
+  measurementId: "G-2CPJ8HH5PC"
+};
